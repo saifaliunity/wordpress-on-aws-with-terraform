d1c2fd0ce0efcc50e2ea4a414db811b68097ee66
saif-qureos <saif@qureos.com>
2022-11-30 03:51:19 +0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -109,7 +109,7 @@ resource "aws_autoscaling_group" "wordpress_asg" {
     }
   launch_template {
     launch_template_specification {
-      id      = aws_launch_template.wordpress_lt.id
+      launch_template_id  = aws_launch_template.wordpress_lt.id
       # version = "$Latest"
     }
   }
