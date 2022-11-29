a31745c63dacc0150b985223e97f097d39564504
saif-qureos <saif@qureos.com>
2022-11-30 03:43:27 +0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -99,8 +99,14 @@ resource "aws_autoscaling_group" "wordpress_asg" {
 
   vpc_zone_identifier = aws_subnet.private_subnets[*].id
   target_group_arns   = [aws_lb_target_group.wordpress_tg.arn]
-  #health_check_type   = "ELB"
+  health_check_type   = "ELB"
 
+  mixed_instances_policy {
+  instances_distribution {
+    on_demand_base_capacity                  = 0
+    on_demand_percentage_above_base_capacity = 25
+    spot_allocation_strategy                 = "capacity-optimized"
+  }
 
   launch_template {
     id      = aws_launch_template.wordpress_lt.id
