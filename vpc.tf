c037215a0ac0f3673c5330e0e5e838ab166cebda
saif-qureos <saif@qureos.com>
2022-12-01 20:09:16 +0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "wordpress_vpc" {
   cidr_block       = "10.0.0.0/24"
   instance_tenancy = "default"
-
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "wordpress-vpc"
     env  = var.env
