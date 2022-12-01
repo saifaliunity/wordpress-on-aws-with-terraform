8b287388798eea660313410ae2ba8d040bf36e30
saif-qureos <saif@qureos.com>
2022-12-01 20:14:43 +0500

    Create vpc-endpoint.tf

---
+++ vpc-endpoint.tf
@@ -0,0 +1,13 @@
+data "aws_region" "current" { }
+
+resource "aws_vpc_endpoint" "efs" {
+  vpc_id            = aws_vpc.wordpress_vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.elasticfilesystem"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = aws_subnet.private_subnets.*.id
+  security_group_ids = [
+    aws_security_group.cardano-node-service_security_group.id,
+  ]
+
+  private_dns_enabled = true
+}
